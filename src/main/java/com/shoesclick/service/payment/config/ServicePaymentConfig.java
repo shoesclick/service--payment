e700e78f098b90bc4fb5c8942c56b2e5c362054f
git <claytnmorais@gmail.com>
2024-03-09 22:47:13 -0300

    inclusao do k8s e dockerfilesp ara pipeline kubernetes

--- src/main/java/com/shoesclick/service/payment/config/ServicePaymentConfig.java
+++ src/main/java/com/shoesclick/service/payment/config/ServicePaymentConfig.java
@@ -6,5 +6,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableConfigurationProperties(MqProperties.class)
-public class OrderApiConfig {
+public class ServicePaymentConfig {
 }
